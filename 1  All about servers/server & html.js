f98a24d51b5be4a193ba4f61a8cf6992944c8a14
sam3569 <smishra9109@gmail.com>
2023-03-16 13:22:34 +0530

    Create a Server in node js and serve html

--- 1  All about servers/server & html.js
+++ 1  All about servers/server & html.js
@@ -1,7 +1,9 @@
 const http = require('http')
 const fs = require('fs')
 
+// create server
 http.createServer((req,res)=>{
+    // change MIME type to 'text/html'
     res.writeHead(200,{"Content-Type":"text/html"})
     fs.readFile('index.html',(err,data)=>{
         if(err) throw err
