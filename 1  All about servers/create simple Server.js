d53d1c697288678a707c0fccae680e21b565b465
sam3569 <smishra9109@gmail.com>
2023-03-16 13:21:56 +0530

    Create a Server in node js

--- 1  All about servers/create simple Server.js
+++ 1  All about servers/create simple Server.js
@@ -1,6 +1,8 @@
 const http = require('http')
 
+// create server
 const server = http.createServer((req,res)=>{
+    // change MIME type to 'text/plain'
     res.writeHead(200,{"Content-Type":"text/plain"})
     console.log('Server started');
     res.end('Working')
@@ -10,9 +12,7 @@ var host = '127.0.0.1'
 var port = 3000
 
 server.listen(port,(err)=>{
-
     if(err) return console.log(err);
-
     console.log('server is listening on ' + host + ':' + port);
 })
 
