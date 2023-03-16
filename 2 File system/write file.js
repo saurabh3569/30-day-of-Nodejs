a35e57512a5b0781019281c4e76305ac24799724
sam3569 <smishra9109@gmail.com>
2023-03-16 16:21:19 +0530

    Writing the file Asynchronously

--- 2 File system/write file.js
+++ 2 File system/write file.js
@@ -1,9 +1,6 @@
 // Writing the file Asynchronously :
-
 const fs = require('fs')
-
 const content = "this is new Content"
-
 fs.writeFile('demo.txt',content,(err)=>{
     if(err) throw err
     else console.log('it\'s saved');
