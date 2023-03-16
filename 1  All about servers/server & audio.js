5abd61f575dbec77a7df1a03f2c8f7ebc3bbd5ed
sam3569 <smishra9109@gmail.com>
2023-03-16 13:22:12 +0530

    Create a Server in node js and serve audio/mp3

--- 1  All about servers/server & audio.js
+++ 1  All about servers/server & audio.js
@@ -1,7 +1,9 @@
 const http = require('http')
 const fs = require('fs')
 
+// create server
 http.createServer((req,res)=>{
+    // change MIME type to 'audio/mp3'
     res.writeHead(200,{"Content-Type":"audio/mp3"})
 
     fs.exists('audio.mp3',function(exists){
