d0749d0f799a9944ab86b5459d7d0d74c9325991
sam3569 <smishra9109@gmail.com>
2023-03-16 16:20:44 +0530

    Reading the file synchronously

--- 2 File system/read file sync.js
+++ 2 File system/read file sync.js
@@ -1,8 +1,5 @@
-// Read a File in Nodejs
-
-const fs = require('fs')
-
 // Reading the file Synchronously :
+const fs = require('fs')
 
 let fileName = 'demo.txt'
 let content = fs.readFileSync(fileName)
